337b004fa34cc904cfd6ac24a3197d0179a9a537
vitormuuniz <vitormuuniz@gmail.com>
2021-03-22 10:01:52 -0300

    finalizing 3rd day

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
+import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdownd";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from "next/head";
-
 import styles from "../styles/pages/Home.module.css";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -21,7 +21,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
